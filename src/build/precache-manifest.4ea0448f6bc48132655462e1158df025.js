9d6fa28da92aa0ed20096423f94393825df60c0e
Rebecca Dittmar <9spire@gmail.com>
2020-05-07 20:17:13 -0700

    first commit

---
+++ src/build/precache-manifest.4ea0448f6bc48132655462e1158df025.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4ee1a349a5066ea515823fe3270a44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "654b297a5fe21be446a5",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "6ac32d2e9b58823ea516",
+    "url": "/static/js/2.aa04e143.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aa04e143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "654b297a5fe21be446a5",
+    "url": "/static/js/main.4be6e999.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  }
+]);
